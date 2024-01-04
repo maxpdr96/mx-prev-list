a6005316fd6db05486a940a2b3b1d7ec4058ed0f
Maxwel <maxpdr96@gmail.com>
2024-01-04 17:06:11 -0300

    Initial commit

---
+++ src/main/java/com/hidarisoft/mxprevlist/MxPrevListApplication.java
@@ -0,0 +1,13 @@
+package com.hidarisoft.mxprevlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxPrevListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxPrevListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hidarisoft/mxprevlist/MxPrevListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hidarisoft.mxprevlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxPrevListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
